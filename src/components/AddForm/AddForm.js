f5f1c6922e21cfe205770b063bb40cf41e91234f
rokas <rokasra@gmail.com>
2022-07-03 16:55:44 +0300

    addForm post

--- src/components/AddForm/AddForm.js
+++ src/components/AddForm/AddForm.js
@@ -8,8 +8,8 @@ import { useAuthCtx } from '../../store/authContext';
 import Button from '../UI/Button/Button';
 
 const initValues = {
-  title: 'A good title',
-  description: 'Even better description',
+  title: '',
+  description: '',
 };
 function AddForm() {
   const history = useHistory();
@@ -18,14 +18,14 @@ function AddForm() {
     initialValues: initValues,
     validationSchema: Yup.object({
       title: Yup.string().max(10).min(2).required(),
-      description: Yup.string().min(4, 'At least 4 symbols are required').max(20).required(),
+      desc: Yup.string().min(4, 'At least 4 symbols are required').max(50).required(),
     }),
     onSubmit: async (values) => {
       console.log('values ===', values);
 
-      const fetchResult = await myFetch(`${baseUrl}/v1/auth/skills`, 'POST', values);
+      const fetchResult = await myFetch(`${baseUrl}/v1/content/skills`, 'POST', values);
       // ar gavom token
-      if (fetchResult.msg === 'Added successfully!') {
+      if (fetchResult.msg === 'Added new skill to account') {
         // turim token
 
         ctx.add(fetchResult.token, values.email);
@@ -38,38 +38,41 @@ function AddForm() {
 
   return (
     <form className={css['add-container']} onSubmit={formik.handleSubmit}>
-      <label htmlFor='email'>Enter Your Email:</label>
-      <input
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        // value={formik.values.title}
-        className={formik.touched.title && formik.errors.title ? css['error-input'] : ''}
-        name='title'
-        type='text'
-        placeholder='Enter Your title'
-        id='title'
-      ></input>
-      {formik.touched.title && formik.errors.title && (
-        <p className={css.errorMsg}>{formik.errors.title}</p>
-      )}
+      <label htmlFor='email'>Enter Your Title:</label>
+      <div className={css['input-group']}>
+        <input
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.title}
+          className={formik.touched.title && formik.errors.title ? css['error-input'] : ''}
+          name='title'
+          type='text'
+          placeholder='Enter Your title'
+          id='title'
+        ></input>
+        {formik.touched.title && formik.errors.title && (
+          <p className={css.errorMsg}>{formik.errors.title}</p>
+        )}
+      </div>
+      <div className={css['input-group']}>
+        <label htmlFor='desc'>Enter Your Description:</label>
+        <textarea
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.desc}
+          className={formik.touched.desc && formik.errors.desc ? css['error-input'] : ''}
+          name='desc'
+          type='text'
+          placeholder='Your Description'
+          // rows='4'
+          // cols='62.9'
+          id='desc'
+        ></textarea>
+        {formik.touched.desc && formik.errors.desc && (
+          <p className={css.errorMsg}>{formik.errors.desc}</p>
+        )}
+      </div>
 
-      <label htmlFor='desc'>Enter Your Description:</label>
-      <textarea
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.desc}
-        className={formik.touched.desc && formik.errors.desc ? css['error-input'] : ''}
-        name='desc'
-        type='text'
-        placeholder='Your Description'
-        rows='4'
-        cols='62.9'
-        id='desc'
-      ></textarea>
-
-      {formik.touched.desc && formik.errors.desc && (
-        <p className={css.errorMsg}>{formik.errors.desc}</p>
-      )}
       <Button submit>Add</Button>
     </form>
   );
